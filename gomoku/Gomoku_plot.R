682c926c835f39cf9eba4011a9761f11192c38b9
Yimo Zhang <yimo_zhang@brown.edu>
2017-12-09 20:03:55 -0500

    Update plot functions.

--- gomoku/Gomoku_plot.R
+++ gomoku/Gomoku_plot.R
@@ -1,60 +1,81 @@
 stage0 = function(){
-  colfunc <- colorRampPalette(c("indianred", "firebrick"))
-  img<-readPNG("gomoku_2.png")
   n = 100
+  x=c(1:n)
+  y=c(1:n)
   par(mar = rep(0, 4))
   plot(1:n, type = "n", xlim = c(1, n), axes = FALSE, xlab = "",
        ylab = "", bty = "o", lab = c(n, n, 1))
-  rasterImage(img,0,0,n,n)
-  text(50,80,label="GOMOKU",cex=5, col = "coral4")
-  text(seq(37, 58, length.out = 6), rep(40, 6), label=unlist(strsplit("BATTLE",NULL)), cex=2,col=colfunc(6))
-  text(seq(34, 66, length.out = 8), rep(20 ,8), label=unlist(strsplit("COMPUTER",NULL)), cex=2,col=colfunc(8))
-  rect(39.7, 33, 59.7, 47)
-  rect(35,12,65,27)
+  taiji = readPNG("taiji.png")
+  windowsFonts(JP1 = windowsFont("Pristina"),
+               JP2 = windowsFont("MS Gothic"),
+               JP3 = windowsFont("Arial Unicode MS"))
+  bg = readJPEG("bg.jpg")
+  colfunc <- colorRampPalette(c("white","goldenrod3", "white","goldenrod3","white"))
+  colfunc1 = colorRampPalette(c("black","gray90"))
+  ggplot(data =NULL,color = "black") + 
+    geom_segment(aes(x, y = rep(1, n), xend = x, yend = rep(n,n)))+
+    geom_segment(aes(x = rep(1,n), y = y, xend = rep(n,n), yend = y))+
+    annotation_custom(rasterGrob(bg, 
+                                 width = unit(1,"npc"), 
+                                 height = unit(1,"npc")), 
+                      -Inf, Inf, -Inf, Inf)+
+    annotate("text", x = seq(33,67, length.out = 6), y=rep(8*n/9,6), col = colfunc(6),
+             size = 14, label = unlist(strsplit("GOMOKU", NULL)), family = "JP1")+
+    annotate("text", x = seq(39,61, length.out = 6), y = rep(28,6), col = colfunc1(6), label = unlist(strsplit("BATTLE",NULL)), family = "JP1", size = 10)+
+    annotate("text",x = seq(33,67, length.out = 8), y = rep(13,8), col = colfunc1(8), family = "JP1", size = 10, label = unlist(strsplit("COMPUTER", NULL)))+
+    annotation_custom(rasterGrob(taiji),xmin = 35, xmax = 65, ymin = 35, ymax = 80)+
+    theme_void()
 }
 
 
 stage1 = function(){
-  colfunc <- colorRampPalette(c("black", "white"))
-  img<-readPNG("gomoku_2.png")
+  taiji = readPNG("taiji.png")
+  windowsFonts(JP1 = windowsFont("Pristina"),
+               JP2 = windowsFont("MS Gothic"),
+               JP3 = windowsFont("Arial Unicode MS"))
+  bg = readJPEG("bg.jpg")
+  colfunc <- colorRampPalette(c("white","goldenrod3", "white","goldenrod3","white"))
+  colfunc1 = colorRampPalette(c("black","gray90"))
   n = 100
-  par(mar = rep(0, 4))
-  plot(1:n, type = "n", xlim = c(1, n), axes = FALSE, xlab = "",
-       ylab = "", bty = "o", lab = c(n, n, 1))
-  rasterImage(img,0,0,n,n)
-  text(50,80,label="Computer",cex=5)
-  text(seq(41, 58, length.out = 5), rep(40, 5), label=unlist(strsplit("BLACK" ,NULL)),cex=2,col=colfunc(5), adj = c(0.5,0.5))
-  text(seq(41, 58, length.out = 5), rep(20, 5), label=unlist(strsplit("WHITE" ,NULL)),cex=2,col=colfunc(5), adj = c(0.7, 0.7))
-  rect(39.7, 32, 59.7, 46)
-  rect(39.7,12,59.7,27)
+  x=c(1:n)
+  y=c(1:n)
+  ggplot(data =NULL,color = "black") + 
+    geom_segment(aes(x, y = rep(1, n), xend = x, yend = rep(n,n)))+
+    geom_segment(aes(x = rep(1,n), y = y, xend = rep(n,n), yend = y))+
+    annotation_custom(rasterGrob(bg, 
+                                 width = unit(1,"npc"), 
+                                 height = unit(1,"npc")), 
+                      -Inf, Inf, -Inf, Inf)+
+    annotate("text", x = seq(26,74, length.out = 8), y=rep(8*n/9,8), col = colfunc(8),
+             size = 14, label = unlist(strsplit("COMPUTER", NULL)), family = "JP1")+
+    annotate("text", x = 50, y = 28, label = "BLACK", col = "black", family = "JP1", size = 10)+
+    annotate("text", x = 50, y = 13, label = "WHITE", col = "white", size = 10, family = "JP1")+
+    annotation_custom(rasterGrob(taiji),xmin = 35, xmax = 65, ymin = 35, ymax = 80)+
+    theme_void()
 }
 
-gameover = function(result){
-  colfunc <- colorRampPalette(c("black", "white"))
-  img1 = readPNG("gomoku_2.png", FALSE)
+gameover = function(result, img){
+  windowsFonts(JP1 = windowsFont("Pristina"),
+               JP2 = windowsFont("MS Gothic"),
+               JP3 = windowsFont("Arial Unicode MS"))
+  bg = readJPEG("bg.jpg")
+  len = str_count(result)
   n = 100
-  par(mar = rep(0, 4))
-  plot(1:n, type = "n", xlim = c(1, n), axes = FALSE, xlab = "",
-       ylab = "", bty = "o", lab = c(n, n, 1))
-  rasterImage(img1,0,0,n,n)
-  rect(0,0,n,n,col = alpha("white", 0.3), border = NA)
-  if(result == "Computer Wins!"){
-    img = readPNG("sad_face.png")
-    rasterImage(img,35,50,65,80)
-  } else {
-    img = readPNG("happy_face.png")
-    rasterImage(img,35,50,65,80)
-  }
-  text(seq(33, 65, length.out = 10), rep(40,10), labels = unlist(strsplit("PLAY AGAIN", NULL)), 
-       cex = 2, adj = c(0.5,0.5),col = colfunc(10))
-  text(seq(43,57, length.out = 4), rep(20,4), labels=unlist(strsplit("QUIT", NULL)), cex=2,adj = c(0.7,0.7), col = colfunc(4))
-  text(50,87,label=result,cex=3,col = "firebrick4")
-  rect(33.7, 32, 65.5, 48)
-  rect(42.3,12,58,27)
-  
-
+  x=c(1:n)
+  y=c(1:n)
+  ggplot(data =NULL,color = "black") + 
+    geom_segment(aes(x, y = rep(1, n), xend = x, yend = rep(n,n)))+
+    geom_segment(aes(x = rep(1,n), y = y, xend = rep(n,n), yend = y))+
+    annotation_custom(rasterGrob(bg, 
+                                 width = unit(1,"npc"), 
+                                 height = unit(1,"npc")), 
+                      -Inf, Inf, -Inf, Inf)+
+    annotate("text", x = 50, y = 8*n/9, label = result, size = 14, col = "white", family = "JP1")+
+    annotate("text", x = 50, y = 28, label = "PLAY AGAIN", col = "black", family = "JP1", size = 10)+
+    annotate("text", x = 50, y = 13, label = "QUIT", col = "white", size = 10, family = "JP1")+
+    annotation_custom(rasterGrob(img),xmin = 35, xmax = 65, ymin = 35, ymax = 80)+
+    theme_void()
 }
 
-w <- matrix(rgb(m[,,1],m[,,2],m[,,3]), nrow=dim(m)[1])
 
 
--- gomoku/gomoku_join.R
+++ gomoku/gomoku_join.R
@@ -1,14 +1,16 @@
-gomoku = function(n = 19){
-  
-  if (.Platform$OS.type == "windows") 
-    x11() else x11(type = "Xlib")
-  
+library(scales)
+library(png)
+library(jpeg)
+
+
+gomoku = function(n)
+{
   click = 0
   result = ""
   
   #Choose whom to play wit
     
-    click = first_choose()
+    click = first_choose(n)
     
     if(click == 1){result = gomoku_self(n)}
     
@@ -19,16 +21,25 @@ gomoku = function(n = 19){
     
       if(click == 3){result = gomoku_computer(n, choose = 1)}
       if(click == 4){result = gomoku_computer(n, choose = 2)}
-      
-    }
-  gameover(result = result)
+      }
+    
+    
+    if(result == "White Wins!"){img = readPNG("white_wins.png")}
+    if(result == "Black Wins!"){img = readPNG("black_wins.png")}
+    if(result == "You Win!"){img = readPNG("happy_face.png")}
+    if(result == "You Lose!"){img = readPNG("sad_face.png")}
+    
+  gameover(result = result, img = img)
   repeat{
     options(locatorBell = FALSE)
     l = locator(1)
     x = l$x
     y = l$y
-    if(x>33.7 & x<65.5 & y>32 & y<48){gomoku(n)}
-    if(x>42.3 & x<58 & y>12 & y<27){return("Game Closed")}
+    if(x>40 & x<60 & y>8 & y<16){
+      dev.off()
+      gomoku(n)
+    }
+    if(x>32 & x<69 & y>23 & y<31){return("Game Closed")}
   }
 }
   
@@ -40,8 +51,8 @@ first_choose = function(){
     l = locator(1)
     x = l$x
     y = l$y
-    if(x>39.7 & x<59.7 & y>33 & y<47){return(1)}
-    if(x>35 & x<65 & y>12 & y<27){return(2)}
+    if(x>37 & x<63 & y>23 & y<31){print(1)}
+    if(x>30 & x<70 & y>8 & y<16){print(2)}
   }
 }
 
@@ -53,8 +64,8 @@ second_choose = function(){
     l = locator(1)
     x = l$x
     y = l$y
-    if(x>39.7 & x<59.7 & y>32 & y<46){return(3)}
-    if(x>39.7 & x<59.7 & y>12 & y<27){return(4)}
+    if(x>40 & x<61 & y>23 & y<31){return(3)}
+    if(x>39 & x<61 & y>8 & y<16){return(4)}
   }
 }
 
