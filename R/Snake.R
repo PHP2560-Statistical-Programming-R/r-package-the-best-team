d613dc458622123a604c8c8d4df3762d87a48c04
Wei Wang <Wayyyy@users.noreply.github.com>
2017-12-15 17:09:55 -0500

    Done

--- R/Snake.R
+++ R/Snake.R
@@ -145,7 +145,7 @@ stage0<-function( ){ ## Startup screen
   rasterImage(bg,0,0,1,1)
   text(0.5,0.7,label="Snake Game",cex=5,col="yellow")
   text(0.5,0.4,label="Any keyboard to start",cex=2,col="pink")
-  text(0.5,0.3,label="up,down,left,right to control direction",cex=2,col="pink")
+  text(0.5,0.3,label="Up,Down,Left,Right to control direction",cex=2,col="pink")
 }
 
 
@@ -156,7 +156,7 @@ stage2<-function( ){  ## Get result
   rasterImage(bgg,0,0,1,1)
   text(0.5,0.7,label="Congratulations!",cex=4,col="pink")
   text(0.5,0.6,label=paste("You have eat",nrow(e$tail),"fruits!"),cex=3,col="pink")
-  text(0.5,0.4,label="'Space'' to restart, 'q' to quit.",cex=2,col="yellow")
+  text(0.5,0.4,label="'Space'' to restart, 'Q' to quit.",cex=2,col="yellow")
 }
 
 
