bd5229e7282b0add329901ace72c3e7003fe0255
Yimo Zhang <yimo_zhang@brown.edu>
2017-12-15 02:42:56 -0500

    Update package.

--- gomoku/gomoku_packages.R
+++
@@ -1,7 +0,0 @@
-#These packages are necessary for the gomoku function to run.
-source("check_packages.R")
-check_packages(c("stringr","ggplot2","Cairo","ggmap","grid","scales","png","jpeg")
-  
-)
-
-
